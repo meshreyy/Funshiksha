2f97a6175e3fd7eba5ea453e2b0950c4aad2e6cd
meshreyy <iamjhashreya@gmail.com>
2025-09-16 09:42:29 +0530

    dropdown with text changing on web

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const App = () => {
-  return <div>Hello Funshiksha</div>;
+
+const translations = {
+  en: { welcome: "Welcome to Funshiksha", save: "Save Progress" },
+  hi: { welcome: "फनशिक्षा में आपका स्वागत है", save: "प्रगति सहेजें" },
+  or: { welcome: "ଫନସିକ୍ଷାକୁ ସ୍ୱାଗତ", save: "ପ୍ରଗତି ସଞ୍ଚୟ କରନ୍ତୁ" }
 };
 
-export default App;
+
+const App: React.FC = () => {
+  const [lang, setLang] = useState('en');
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setLang(e.target.value);
+  };
+
+  return (
+    <div>
+      <h1>{translations[lang].welcome}</h1>
+      <select value={lang} onChange={handleChange}>
+         <option value="en">English</option>
+        <option value="hi">Hindi</option>
+        <option value="or">Odia</option>
+      </select>
+      <button>{translations[lang].save}</button>
+    </div>
+  )
+
+};
+
+
+
+
+  export default App;
