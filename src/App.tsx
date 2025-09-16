ca4c952734e61131292732c5e54a712602173aaf
meshreyy <iamjhashreya@gmail.com>
2025-09-16 09:32:51 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+  return <div>Hello Funshiksha</div>;
+};
+
+export default App;
---
+++ src/serviceWorkerRegistration.ts
@@ -0,0 +1,9 @@
+export function register() {
+  // Minimal register, can expand later
+  console.log('Service Worker registered (stub)');
+}
+
+export function unregister() {
+  // Minimal unregister
+  console.log('Service Worker unregistered (stub)');
+}
